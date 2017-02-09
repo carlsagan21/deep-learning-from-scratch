668403b69b8775526ebe72d47a8a871ef2380581
soo <carlsagan96@gmail.com>
2017-02-09 18:28:58 +0900

    ~136p

--- main.py
+++ main.py
@@ -89,3 +89,75 @@ def cross_entropy_error(y, t):
 
     batch_size = y.shape[0]
     return -np.sum(t * np.log(y)) / batch_size
+
+
+def numerical_diff(f, x):
+    h = 1e-4 # 0.0001
+    return (f(x + h) - f(x - h)) / (2 * h)
+
+
+def numerical_gradient(f, x):
+    h = 1e-4
+    grad = np.zeros_like(x)
+
+    for idx in range(x.size):
+        tmp_val = x[idx]
+        x[idx] = tmp_val + h
+        fxh1 = f(x)
+
+        x[idx] = tmp_val - h
+        fxh2 = f(x)
+
+        grad[idx] = (fxh1 - fxh2) / (2 * h)
+        x[idx] = tmp_val
+
+    return grad
+
+
+def gradient_descent(f, init_x, lr=0.01, step_num=100):
+    x = init_x
+
+    for i in range(step_num):
+        grad = numerical_gradient(f, x)
+        x -= lr * grad
+
+    return x
+
+
+def function_2(x):
+    return x[0]**2 + x[1]**2
+
+init_x = np.array([-3.0, 4.0])
+# print(gradient_descent(function_2, init_x=init_x, lr=1, step_num=100))
+
+class simpleNet:
+    def __init__(self):
+        self.W = np.random.randn(2,3)
+
+    def predict(self, x):
+        return np.dot(x, self.W)
+
+    def loss(self, x, t):
+        z = self.predict(x)
+        y = softmax(z)
+        loss = cross_entropy_error(y, t)
+
+        return loss
+
+net = simpleNet()
+print(net.W)
+
+x = np.array([0.6, 0.9])
+p = net.predict(x)
+print(np.argmax(p))
+
+t = np.array([0, 0, 1])
+print(net.loss(x, t))
+
+
+def f():
+    return net.loss(x, t)
+
+
+dW = numerical_gradient(f, net.W)
+print(dW)
